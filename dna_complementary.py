3d6c95ea56d711fd17d37088aa94818a588c2893
simutek <simutek@Xavier-Mac-3.local>
2024-06-08 21:38:40 -0700

    feat: defines the original DNA sequence as upper letters

--- dna_complementary.py
+++ dna_complementary.py
@@ -5,5 +5,8 @@ def main():
 
     complementary_seq= get_complementary(sequence) #write a function that finds the complement for each base
 
-     """
+    """
+    
+def input_sequence():
+    return input(" Enter your DNA sequence: ").upper() #makes sure that all the bases are upper letters
     
