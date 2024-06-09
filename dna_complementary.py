848067a9390efe0ff3d25ea343466f57a098c9fa
simutek <simutek@Xavier-Mac-3.local>
2024-06-08 21:29:26 -0700

    feat: main function draft and logic for DNA complementary sequence generation

---
+++ dna_complementary.py
@@ -0,0 +1,9 @@
+
+#we define the main function and logic
+def main():     
+    """ sequence = input_sequence() #ask the user for the DNA sequence (ACGT) 
+
+    complementary_seq= get_complementary(sequence) #write a function that finds the complement for each base
+
+     """
+    
