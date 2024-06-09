a24b8c7d0df344cbfeac498a1a79493367864a2f
simutek <simutek@Xavier-Mac-3.local>
2024-06-08 21:44:09 -0700

    feat: defines the complementary pair bases DNA

--- dna_complementary.py
+++ dna_complementary.py
@@ -4,9 +4,12 @@ def main():
     """ sequence = input_sequence() #ask the user for the DNA sequence (ACGT) 
 
     complementary_seq= get_complementary(sequence) #write a function that finds the complement for each base
+    
 
     """
     
 def input_sequence():
     return input(" Enter your DNA sequence: ").upper() #makes sure that all the bases are upper letters
-    
+
+def get_complementary(sequence):
+    complement = {"A":"T", "C":"G", "G":"C", "T":"A"}
